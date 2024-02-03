cc9db98cc147df83d74949feeb4533024a7038be
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2024-02-03 21:19:24 +0530

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
+
+import tunnel from 'astro-tunnel';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), tunnel()],
+  adapter: netlify({
+    imageCDN: true,
+    cacheOnDemandPages: true,
+  }),
+  prefetch: {
+    defaultStrategy: 'hover',
+  },
+  output: 'server',
+});
