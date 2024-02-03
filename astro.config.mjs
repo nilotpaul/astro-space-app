0014b9018672da38a246c3887a02e58dd4552b95
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2024-02-03 21:22:38 +0530

    fix(astro.config.mjs): removed tunnel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 
-import tunnel from 'astro-tunnel';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), tunnel()],
+  integrations: [tailwind()],
   adapter: netlify({
     imageCDN: true,
     cacheOnDemandPages: true,
